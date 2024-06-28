fe2688529bc3d0526b75835bfe389862c5b7ef58
LapTred <braulio_damian@hotmail.com>
2024-06-28 12:25:52 -0700

    Footer y Registro
    Cambios en el footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/navbar/Navbar.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home.js";
-import About from "./pages/about/About.js";
-import Specialty from "./pages/specialty/Specialties.js";
+import Home from "./Components/home/Home.js";
+import About from "./Components/about/About.js";
+import Specialty from "./Components/specialty/Specialties.js";
 import Footer from './Components/footer/Footer.js'; 
 import Login from "./Components/login/Login.js"
 
@@ -14,7 +14,7 @@ const App = () => {
      <Navbar />
      <main className="main-content">
        <Routes>
-         <Route exact path="/" element={<Home />} />
+         <Route exact path="/" element={<About />} />
          <Route exact path="/about" element={<About />} />         
          <Route exact path="/especialidades" element={<Specialty />} />
          <Route exact path="/register" element={<Login />} />
